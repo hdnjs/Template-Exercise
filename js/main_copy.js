00a5ffc0ef7e367556dd62cc99249dcf010415d4
hdnjs <khw734100@gmail.com>
2022-11-24 11:04:56 +0900

    1124

--- js/main_copy.js
+++ js/main_copy.js
@@ -1,10 +1,3 @@
-//  1. Sticky Header : 스크롤 시 헤더가 따라다니는 기능
-//  2. Header Menu Toggle
-//  3. Navigation Menu Design : 해당 섹션에 네비게이션 활성화 및 디자인 변경
-//  4. Question Section Accordion
-//  5. Reveal Effect
-//  6. Mobile Navigation Design
-
 // ========== Header Sticky ===========
 const header = document.querySelector(".header");
 const stickyHeader = () => {
@@ -25,10 +18,10 @@ const activateNavMenu = () => {
   const scrY = window.scrollY;
 
   sections.forEach((section) => {
-    const secHeight = section.offsetHeight; //  offsetHeight : section 요소의 높이값
-    const secTop = section.offsetTop - headerHeight; //  offsetTop : section 요소의 윗 쪽 offset 값
+    const secHeight = section.offsetHeight;
+    const secTop = section.offsetTop - headerHeight;
 
-    const secId = section.getAttribute("id"); //  각 섹션의 아이디값 지정
+    const secId = section.getAttribute("id");
 
     if (scrY > secTop && scrY <= secHeight + secTop) {
       document.querySelector(`.nav-${secId}`).classList.add("active");
@@ -76,44 +69,20 @@ const hideMenu = () => {
 };
 
 const toggleMenu = (e) => {
-  // console.log(this);  // 화살표 함수는 this에 전역 객체 window를 호출함
-  //   console.log(e.target);
   e.preventDefault();
   const target = e.currentTarget;
 
   target.classList.toggle("close");
 
   if (target.classList.contains("close")) {
-    // target.firstElementChild.setAttribute("class", "ri-close-line");
-    // target.children[0].setAttribute("class", "ri-close-line");
-    // mobileMenu.classList.add("show");
-    // body.style.overflow = "hidden";
-
-    // if (scrY === 0) {
-    //   header.style.boxShadow = "0 2px 2px #efefef";
-    // }
-
     showMenu();
   } else {
-    // target.firstElementChild.setAttribute("class", "ri-menu-line");
-    // target.children[0].setAttribute("class", "ri-menu-line");
-    // mobileMenu.classList.remove("show");
-    // body.style.overflow = "auto";
-
-    // if (scrY === 0) {
-    //   header.setAttribute("style", "");
-    // }
     hideMenu();
   }
 };
 
 body.addEventListener("click", (e) => {
-  // console.log(e.target);
   if (!header.contains(e.target)) {
-    // console.log(true);
-    // menu.children[0].setAttribute("class", "ri-menu-line");
-    // mobileMenu.classList.remove("show");
-    // body.style.overflow = "auto";
     hideMenu();
   }
 });
@@ -125,7 +94,6 @@ menu.addEventListener("click", toggleMenu);
 const accHeaders = document.querySelectorAll(".question-box");
 
 const activateAccordion = (e) => {
-  // console.log(e.currentTarget);
   const $this = e.currentTarget;
   $this.classList.toggle("active");
 };
@@ -147,6 +115,4 @@ sr.reveal(".home-box");
 sr.reveal(".about-box");
 sr.reveal(".middle-box", { interval: 200 });
 sr.reveal(".item", { interval: 200 });
-
-// sr.reveal(".home-box", { duration: 1000, origin: "bottom", distance: "50px" });
 // =====================================
